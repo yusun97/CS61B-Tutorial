55d79458440a8c821e5299cb44fd4bfb5a808966
yusun97 <yusun.apply@outlook.com>
2023-01-11 23:36:08 -0800

    complete readBodies method

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -7,10 +7,31 @@ public class NBody{
 		return in.readDouble();
 	}
 
-	public static double readRadius(String fileName){
+	public static Body[] readBodies(String fileName){
 		In in = new In(fileName);
 		in.readInt();
-		return in.readDouble();
+		in.readDouble();
+
+		Body[] bodys = new Body[5];
+
+		/*
+		 The first two values are the x- and y-coordinates of the initial position; 
+		 the next pair of values are the x- and y-components of the initial velocity; 
+		 the fifth value is the mass; 
+		 the last value is a String that is the name of an image file used to display the planets.
+		*/
+
+		for(int i = 0; i < 5 && !in.isEmpty(); i++){
+			double xxPos = in.readDouble();
+			double yyPos = in.readDouble();
+			double xxVel = in.readDouble();
+			double yyVel = in.readDouble();
+			double mass = in.readDouble();
+			String imgFileName = in.readString();
+			bodys[i] = new Body(xxPos, yyPos, xxVel, yyVel, mass, imgFileName);
+
+		}
+		return bodys;
 	}
 
 
